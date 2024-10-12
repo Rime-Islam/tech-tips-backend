6b10a0b38f083cc9bcaddb3b5ebd929d69221cd5
rime-islam <rimeislam672@gmail.com>
2024-10-13 00:38:47 +0600

    post create

--- src/app/modules/Post/post.service.ts
+++ src/app/modules/Post/post.service.ts
@@ -1,6 +1,9 @@
 import { Schema } from "mongoose"
 import { TPost } from "./post.interfase";
 import { Post } from "./post.model";
+import { User } from "../User/user.model";
+import AppError from "../../Error/AppError";
+import httpStatus from "http-status";
 
 
 
@@ -17,19 +20,78 @@ const CreatePost = async (payload: TPost) => {
 const getAllPostDB = async () => {
     const result = await Post.find()
     .populate("user")
-    .populate("comment.user")
+    .populate("comments.user")
     .sort({ createdAt: -1, updatedAt: -1, upvotesCount: -1 });
     return result;
 };
 
+const getSinglePostDB = async (id: string) => {
+    const result = await Post.findById(id).populate("user").populate("comments.user");
+    return result;
+};
 
+const getMyPostDB = async (email: string) => {
+    const filterUser = await User.findOne({ email });
+    const findUser = filterUser?._id;
+    const result = await Post.find({
+        user: findUser,
+    }).populate("user")
+    .populate("comments.user")
+    .sort({ createdAt: -1, updatedAt: -1 });
+    return result;
+};
 
+const updateMyPostDB = async ( id: string, email: string, payload: TPost) => {
+    const findPost = await User.findById(id);
+    const findUser = await User.findOne({ email });
 
+    if (!findUser) {
+        throw new AppError(httpStatus.NOT_EXTENDED, "User not found");
+    }
 
+    if (findPost && findUser) {
+        if (findPost.user.toString() !== findUser._id!.toString()) {
+            throw new AppError(httpStatus.CONFLICT, "This post is not yours");
+        }
+    }
 
+    const result = await Post.findByIdAndUpdate(id, payload, {
+        new: true,
+        runValidators: true,
+    }).populate("user")
+    return result;
+};
+
+const getPostByCategory = async (id: unknown) => {
+    const result = await Post.find({ category: id })
+    .populate("user")
+    .populate("comments.user")
+    return result;
+};
+
+const deleteMyPostDB = async ( id: string, email: string, payload: TPost) => {
+    const findPost = await Post.findById(id);
+    const findUser = await User.findOne({ email });
+
+    if (!findUser) {
+        throw new AppError(httpStatus.NOT_EXTENDED, "User not found");
+    }
+    if (findPost && findUser) {
+        if (findPost.user.toString() !== findUser._id!.toString()) {
+            throw new AppError(httpStatus.CONFLICT, "This post is not yours");
+        }
+    }
+    const result = await Post.findByIdAndDelete(id)
+    return result;
+};
 
 
 export const PostService = {
     CreatePost,
     getAllPostDB,
+    getSinglePostDB,
+    getMyPostDB,
+    getPostByCategory,
+    updateMyPostDB,
+    deleteMyPostDB,
 };
